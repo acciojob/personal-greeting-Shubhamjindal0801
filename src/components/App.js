7176c6352460ff5f71847e9fb88608770ba440d8
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-05-18 00:47:23 +0530

    submitting code-Thu May 18 2023 00:47:18 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [value,setVal] = useState(" ");
+  useEffect(()=>{
+    if(value.trim()!==""){
+      document.getElementById("result").textContent = `Hello ${value}!`
+    }
+    else{
+      document.getElementById("result").textContent = ``
+    }
+  },[value])
   return (
     <div>
         {/* Do not remove the main div */}
+        <h2>Enter your name:</h2>
+        <input type="text" onChange={(e)=>setVal(e.target.value)} />
+        <p id="result"></p>
     </div>
   )
 }
